07a4f66048153a48e236d8f0711cb14b05a12b2e
Javad Rajabzade <56496801+Ja7adR@users.noreply.github.com>
2021-01-08 09:42:55 +0330

    Please restart you --> your

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     sudo cp -r /usr/share/X11/xkb/symbols/ir /usr/share/X11/xkb/symbols/ir.bak
     sudo cp -r ir /usr/share/X11/xkb/symbols/ir
-    echo "Your keyboard set for pesian style windows, Please restart you ubuntu for change do it."
+    echo "Your keyboard set for pesian style windows, Please restart your ubuntu for change do it."
 else
     exit
 fi
