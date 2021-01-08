672f750552260ccad4b1cb2b7e313fbd60bf0952
Javad <ja7adr@gmail.com>
2021-01-08 09:38:12 +0330

    add backup from ir file first

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ clear
 echo -n "Are you want change keyboard style to persian windows (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
+    sudo cp -r /usr/share/X11/xkb/symbols/ir /usr/share/X11/xkb/symbols/ir.bak
     sudo cp -r ir /usr/share/X11/xkb/symbols/ir
     echo "Your keyboard set for pesian style windows, Please restart you ubuntu for change do it."
 else
